2d64de4288084da06dcdcd09b437dc7e084a69bc
cfgarcia654 <cfgarcia654@misena.edu.co>
2023-09-06 12:14:20 -0500

    nuevo producto

--- app/products/routes.py
+++ app/products/routes.py
@@ -1,8 +1,31 @@
 from flask import render_template
 from . import products
 from .forms import NewProductForm
+import app
+import os
 
-@products.route('/create')
+
+@products.route('/create', methods=["GET", "POST"])
 def crear_producto():
+    p = app.models.Producto()
     form = NewProductForm()
+    if form.validate_on_submit():
+        #llenar atibutes
+        #del obgeto producto
+        #co el formulaio
+        form.populate_obj(p)
+        #registrar en base de dato
+        app.db.session.add(p)
+        p.imagen = form.imagen.data.filename
+        app.db.session.commit()
+        
+        archivo = form.imagen.data
+        archivo.save( 
+             os.path.abspath(
+                 os.getcwd() +
+                 '/app/products/imagenes/'
+                 + p.imagen)
+             )
+        return os.getcwd()
+    
     return render_template('new.html', form = form) 
